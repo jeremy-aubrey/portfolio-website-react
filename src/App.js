72e45b59f88b756f40e7d6a80b8b946ee94230e1
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-25 11:00:52 -0600

    added routes for about, projects, tools, and redirects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import './App.css';
+import AboutPage from "./pages/About";
+import ProjectsPage from "./pages/Projects";
+import ToolsPage from "./pages/Tools";
 
-const App = () =>  {
-  return <h1>Hello World</h1>
-}
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<AboutPage />}></Route>
+        <Route path="/projects" element={<ProjectsPage />}></Route>
+        <Route path="/tools" element={<ToolsPage />}></Route>
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
