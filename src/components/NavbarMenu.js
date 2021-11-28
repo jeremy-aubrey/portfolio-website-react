b87e9737d2f2ef18861d4c3907f4e2b258788b23
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-11-28 00:01:02 -0600

    added more margin to navbar

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -6,12 +6,12 @@ const NavbarMenu = () => {
   return (
     <Navbar expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#home" className="text-light">
+        <Navbar.Brand href="#home" className="text-light mt-4 ms-3">
           Jeremy Aubrey
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-aut ms-auto">
+          <Nav className="ms-auto mt-4 me-3">
             <Nav.Link href="/about" className="text-light">
               About
             </Nav.Link>
